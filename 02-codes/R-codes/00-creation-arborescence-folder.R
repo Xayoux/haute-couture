aa67464865763200223ea2e2227dd691d28a4fb4
Romain Capliez <romain.capliez01@gmail.com>
2024-04-08 16:48:06 +0200

    Création du code pour créer l'arborescence du dossier copmme il faut pour les output

---
+++ 02-codes/R-codes/00-creation-arborescence-folder.R
@@ -0,0 +1,20 @@
+library(here)
+dir.create(
+  here("03-processed-data", "01-dataframes"),
+  recursive = TRUE)
+
+dir.create(
+  here("04-output","01-graphs","01-tests"),
+  recursive = TRUE)
+
+dir.create(
+  here("04-output","01-graphs","02-finals"),
+  recursive = TRUE)
+
+dir.create(
+  here("04-output","02-tables","01-tests"),
+  recursive = TRUE)
+
+dir.create(
+  here("04-output","02-tables","02-finals"),
+  recursive = TRUE)
