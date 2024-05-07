4c483c3be56b4832d7a7e4373cb81029e28dcd4d
Romain Capliez <romain.capliez01@gmail.com>
2024-05-07 11:41:24 +0200

    Move exploration outliers et seuils dans data-exploration.R et ajout du graphique d'exportation sans secteur

--- 02-codes/R-codes/analyse.R
+++ 02-codes/R-codes/analyse.R
@@ -50,267 +50,10 @@ df_product <-
 
 remove(chapter_codes)
 
-# Télécahrger la base de données BACI -------------------------------------
+# Télécharger la base de données BACI -------------------------------------
 # dl_baci(
 #   dl_folder = path_baci_folder_origine, rm_csv = TRUE
 # )
-
-
-# Analyse des outliers -----------------------------------------------------
-
-# Définition des seuils pour les méthodes classiques et fh13
-seuil_H_vector <- c(0.99, 0.975, 0.95, 0.90)
-seuil_L_vector <- c(0.01, 0.025, 0.05, 0.10)
-
-# Définition des seuils pour la méthode sd
-seuil_H_vector_sd <- c(1, 2, 3, 4)
-seuil_L_vector_sd <- c(1, 2, 3, 4)
-
-# Analyse des share du commerce sans outliers avec la méthode classic
-list_eval_outliers_classic <- 
-  eval_outliers_share(
-    baci = path_baci_folder_parquet_origine,
-    years = 2010:2022,
-    codes = unique(df_product$HS92),
-    method = "classic",
-    seuil_H_vector = seuil_H_vector,
-    seuil_L_vector = seuil_L_vector,
-    graph = TRUE,
-    path_graph_output = here(path_graphs_exploration_folder, 
-                             "outliers-methode-classic.png")
-  )
-
-gc()
-
-# Analyse de la distribution des diff de vu avec la méthode classic
-eval_outliers_dist(
-  baci = path_baci_folder_parquet_origine,
-  years = 2010:2022,
-  codes = unique(df_product$HS92),
-  method = "classic",
-  seuil_H_vector = seuil_H_vector,
-  seuil_L_vector = seuil_L_vector,
-  graph_type = "density",
-  ref = "kt",
-  wrap = TRUE,
-  print = TRUE,
-  output_type = "xlsx",
-  path_output = here(path_df_exploration_folder, "test-outliers-classic.xlsx") 
-)
-
-gc()
-
-# Analyse des share du commerce sans outliers avec la méthode fh13
-list_eval_outliers_fh13 <- 
-  eval_outliers_share(
-    baci = path_baci_folder_parquet_origine,
-    years = 2010:2022,
-    codes = unique(df_product$HS92),
-    method = "fh13",
-    seuil_H_vector = seuil_H_vector,
-    seuil_L_vector = seuil_L_vector,
-    graph = TRUE,
-    path_graph_output = here(path_graphs_exploration_folder, 
-                             "outliers-methode-fh13.png")
-  )
-
-gc()
-
-# Analyse de la distribution des diff de vu avec la méthode fh13
-eval_outliers_dist(
-  baci = path_baci_folder_parquet_origine,
-  years = 2010:2022,
-  codes = unique(df_product$HS92),
-  method = "fh13",
-  seuil_H_vector = seuil_H_vector,
-  seuil_L_vector = seuil_L_vector,
-  graph_type = "density",
-  ref = "kt",
-  wrap = TRUE,
-  print = TRUE,
-  output_type = "xlsx",
-  path_output = here(path_df_exploration_folder, "test-outliers-fh13.xlsx") 
-)
-
-gc()
-
-# Analyse des share du commerce sans outlier avec la méthode sd
-list_eval_outliers_sd <- 
-  eval_outliers_share(
-    baci = path_baci_folder_parquet_origine,
-    years = 2010:2022,
-    codes = unique(df_product$HS92),
-    method = "sd",
-    seuil_H_vector = seuil_H_vector_sd,
-    seuil_L_vector = seuil_L_vector_sd,
-    graph = TRUE,
-    path_graph_output = here(path_graphs_exploration_folder, 
-                             "outliers-methode-sd.png")
-  )
-
-gc()
-
-# Analyse de la distribution des diff de vu avec la méthode sd
-eval_outliers_dist(
-  baci = path_baci_folder_parquet_origine,
-  years = 2010:2022,
-  codes = unique(df_product$HS92),
-  method = "sd",
-  seuil_H_vector = seuil_H_vector_sd,
-  seuil_L_vector = seuil_L_vector_sd,
-  graph_type = "density",
-  ref = "kt",
-  wrap = TRUE,
-  print = TRUE,
-  output_type = "xlsx",
-  path_output = here(path_df_exploration_folder, "test-outliers-sd.xlsx") 
-)
-
-# Analyse des share du commerce sans outlier avec la méthode be11
-list_eval_outliers_be11 <-
-  eval_outliers_share(
-    baci = path_baci_folder_parquet_origine,
-    years = 2010:2022,
-    codes = unique(df_product$HS92),
-    method = "be11",
-    seuil_H_vector = c(80, 100),
-    seuil_L_vector = c(80, 100),
-    graph = TRUE,
-    path_graph_output = here(path_graphs_exploration_folder,
-                             "outliers-methode-be11.png")
-  )
-
-gc()
-
-# Analyse de la distribution des diff de vu avec la méthode be11
-eval_outliers_dist(
-  baci = path_baci_folder_parquet_origine,
-  years = 2010:2022,
-  codes = unique(df_product$HS92),
-  method = "be11",
-  seuil_H_vector = 100,
-  seuil_L_vector = 100,
-  graph_type = "density",
-  ref = "kt",
-  wrap = TRUE,
-  print = TRUE,
-  output_type = "xlsx",
-  path_output = here(path_df_exploration_folder, "test-outliers-be11.xlsx")
-)
-
-remove(seuil_H_vector, seuil_L_vector, seuil_H_vector_sd, seuil_L_vector_sd,
-       list_eval_outliers_classic, list_eval_outliers_fh13, list_eval_outliers_sd,
-       list_eval_outliers_be11)
-
-gc()
-
-# Exploration des != seuils sur le nb de produits et de concu ---------------
-# Importer fonction pour faire l'exploration des seuils
-source(here("02-codes", "R-codes", "01-exploration-seuils-function.R"))
-
-# Définition de plusieurs seuils différents : méthode fontagné 1997
-seuils_haut_gamme <- c(1.15, 1.25, 1.5, 1.75, 2, 2.5, 2.75, 3)
-
-# Outliers définis comme 1% et 99%
-exploration_haut_gamme_func(
-  baci = path_baci_folder_parquet_origine,
-  ponderate = "q",
-  years = 2010,
-  codes = unique(df_product$HS92),
-  method_outliers = "classic",
-  seuil_H_outliers = 0.99,
-  seuil_L_outliers = 0.01,
-  alpha_H_gammes = seuils_haut_gamme,
-  seuil_2_gammes = 0.75,
-  doc_title = str_glue("products-nb-concu-fontagne1997-outliers-classic99-seuil2-")
-)
-
-gc()
-
-# Outliers définis comme 5% et 95%
-exploration_haut_gamme_func(
-  baci = path_baci_folder_parquet_origine,
-  ponderate = "q",
-  years = 2010,
-  codes = unique(df_product$HS92),
-  method_outliers = "classic",
-  seuil_H_outliers = 0.95,
-  seuil_L_outliers = 0.05,
-  alpha_H_gammes = seuils_haut_gamme,
-  seuil_2_gammes = 0.75,
-  doc_title = str_glue("products-nb-concu-fontagne1997-outliers-classic95-seuil2-")
-)
-
-gc()
-
-# Outliers définis comme supérieurs à 1 sd
-exploration_haut_gamme_func(
-  baci = path_baci_folder_parquet_origine,
-  ponderate = "q",
-  years = 2010,
-  codes = unique(df_product$HS92),
-  method_outliers = "sd",
-  seuil_H_outliers = 1,
-  seuil_L_outliers = 1,
-  alpha_H_gammes = seuils_haut_gamme,
-  seuil_2_gammes = 0.75,
-  doc_title = str_glue("products-nb-concu-fontagne1997-outliers-sd1-seuil2-")
-)
-
-gc()
-
-# Outliers définis comme supérieurs à 2 sd
-exploration_haut_gamme_func(
-  baci = path_baci_folder_parquet_origine,
-  ponderate = "q",
-  years = 2010,
-  codes = unique(df_product$HS92),
-  method_outliers = "sd",
-  seuil_H_outliers = 2,
-  seuil_L_outliers = 2,
-  alpha_H_gammes = seuils_haut_gamme,
-  seuil_2_gammes = 0.75,
-  doc_title = str_glue("products-nb-concu-fontagne1997-outliers-sd2-seuil2-")
-)
-
-gc()
-
-# Outliers définis comme supérieurs à 3 sd
-exploration_haut_gamme_func(
-  baci = path_baci_folder_parquet_origine,
-  ponderate = "q",
-  years = 2010,
-  codes = unique(df_product$HS92),
-  method_outliers = "sd",
-  seuil_H_outliers = 3,
-  seuil_L_outliers = 3,
-  alpha_H_gammes = seuils_haut_gamme,
-  seuil_2_gammes = 0.75,
-  doc_title = str_glue("products-nb-concu-fontagne1997-outliers-sd3-seuil2-")
-)
-
-# Outliers définis avec méthode be11
-exploration_haut_gamme_func(
-  baci = path_baci_folder_parquet_origine,
-  ponderate = "q",
-  years = 2010,
-  codes = unique(df_product$HS92),
-  method_outliers = "be11",
-  seuil_H_outliers = 100,
-  seuil_L_outliers = 100,
-  alpha_H_gammes = seuils_haut_gamme,
-  seuil_2_gammes = 0.75,
-  doc_title = str_glue("products-nb-concu-fontagne1997-outliers-be11-seuil2-")
-)
-
-remove(
-  exploration_seuil_haut_gamme, df_nb_concu_by_seuil, df_nb_product_by_seuil,
-  part_produit_total_function, file_exploration_seuils_function, 
-  exploration_haut_gamme_func, seuils_haut_gamme
-)
-
-gc()
-
 
 # Regarder nb produits en dynamique ---------------------------------------
 source(here("02-codes", "R-codes", "02-nb-products-HG-by-years.R"))
@@ -475,7 +218,75 @@ print(graph)
 
 # Sauvegarder le graphique
 ggsave(
-  here(path_graphs_exploration_folder, "market_share-hg-exporter-regions-good.png"), 
+  here(path_graphs_exploration_folder, "market_share-hg-exporter-regions-sector.png"), 
   graph, width = 15, height = 8
 )
 
+
+# Créer le graph représentant les exportateurs (pays/régions) au total
+graph <- 
+  path_baci_processed |> 
+  open_dataset() |> 
+  collect() |> 
+  # Définir les pays et régions concurrents (provient de l'analyse exploratoire des régions pour l'export)
+  mutate(
+    exporter_name_region = 
+      case_when(
+        exporter == "FRA" ~ "France",
+        exporter == "ITA" ~ "Italie",
+        exporter == "CHN" ~ "Chine",
+        exporter == "CHE" ~ "Suisse",
+        exporter == "TUR" ~ "Turquie",
+        exporter_name_region == "North America" ~ "Amérique",
+        exporter_name_region == "South America, Central America and Caribbean" ~ "Amérique",
+        exporter == "GBR" ~ "Reste de Union européenne",
+        exporter_name_region == "European Union" ~ "Reste de Union européenne",
+        exporter %in% c("HKG", "JPN", "KOR") ~ "Japon, Corée, Hong Kong",
+        exporter_name_region %in% c("South-East Asia", "South Asia and Pacific") ~ "Reste de l'Asie",
+        exporter_name_region == "Near and Middle East" ~ "Moyen-Orient",
+        .default = "Reste du monde"
+      ),
+    # Appliquer l'ordre d’apparition aux régions
+    exporter_name_region = factor(exporter_name_region, levels = ordre_pays)
+  ) |> 
+  arrow_table() |> 
+  # Calculer les parts de marché sur ces nouvelles régions au total
+  market_share(
+    summarize_k = "t",
+    summarize_v = "exporter_name_region",
+    by = NULL,
+    seuil = 0,
+    years = 2010:2022,
+    codes = unique(df_products_HG$k),
+    path_output = NULL,
+    return_output = TRUE,
+    return_pq = FALSE
+  ) |> 
+  # Créer le graphique
+  ggplot(aes(x = t, y = market_share_t_k_i, fill = exporter_name_region)) +
+  geom_area() +
+  scale_x_continuous(breaks = seq(2010, 2022, 2)) +
+  scale_y_continuous(labels = label_percent(scale = 1)) +
+  # scale_fill_brewer(palette = "Paired") +
+  scale_fill_manual(values = couleurs_pays) +
+  labs(
+    x = "Année",
+    y = "Part de marché",
+    title = "Exportations haut de gamme",
+    fill = ""
+  ) +
+  theme_bw()+
+  theme(
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_blank(),
+    strip.background = element_rect(colour = "black", fill = "#D9D9D9"),
+    axis.text.x = element_text(angle = 45, hjust = 1)
+  )
+
+print(graph)
+
+# Sauvegarder le graphique
+ggsave(
+  here(path_graphs_exploration_folder, "market_share-hg-exporter-regions-no-sector.png"), 
+  graph, width = 15, height = 8
+)
